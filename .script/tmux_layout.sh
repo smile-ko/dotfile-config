9b97c91df2d431f8209997a0bc4ffa71dadb620f
Vũ Đức Tiến <tien.vu@edn.com.vn>
2025-02-23 13:31:20 +0700

    feat: upload dotfile

---
+++ .script/tmux_layout.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+tmux split-window -h -l 30%
+tmux select-pane -t 1
+tmux split-window -v -l 66%
+tmux split-window -v
+tmux select-pane -t 0
+clear
---
+++ nvim/lua/config/autocmds.lua
@@ -0,0 +1,20 @@
+-- Turn off paste mode when leaving insert
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  command = "set nopaste",
+})
+
+-- Disable the concealing in some file formats
+-- The default conceallevel is 3 in LazyVim
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "json", "jsonc", "markdown" },
+  callback = function()
+    vim.opt.conceallevel = 0
+  end,
+})
+
+-- Set cursor default when leave vim
+vim.api.nvim_create_autocmd("VimLeave", {
+  pattern = "*",
+  command = "set guicursor=a:ver25",
+})
