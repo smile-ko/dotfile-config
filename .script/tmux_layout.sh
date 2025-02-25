795b504a890f53c7f673184f027f2e2c61986db3
Vũ Đức Tiến <tien.vu@edn.com.vn>
2025-02-25 16:14:21 +0700

    update: update shell

--- .script/tmux_layout.sh
+++ .script/tmux_layout.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 tmux split-window -h -l 30%
+tmux select-pane -t 0
+clear
 tmux select-pane -t 1
 tmux split-window -v -l 66%
 tmux split-window -v
-tmux select-pane -t 0
-clear
+tmux select-pane -t 1
