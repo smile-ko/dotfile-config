5406cdf995996e043433267c1ee15cb637f5aa7d
Vũ Đức Tiến <tien.vu@edn.com.vn>
2025-03-21 13:02:06 +0700

    update: avante.nvim using model claude

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -4,15 +4,13 @@ return {
   version = false, -- Never set this value to "*"! Never!
   opts = {
     -- add any opts here
-    -- for example
-    provider = "openai",
-    openai = {
-      endpoint = "https://api.openai.com/v1",
-      model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-      timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+    provider = "claude", -- Recommend using Claude
+    claude = {
+      endpoint = "https://api.anthropic.com",
+      -- model = "claude-3-5-sonnet-20241022",
+      model = "claude-3-haiku-20240307",
       temperature = 0,
-      max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-      --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+      max_tokens = 4096,
     },
     mappings = {
       ask = "<leader>ua", -- ask
