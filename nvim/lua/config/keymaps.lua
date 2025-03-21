28001b93fb8979e2dad4423bc2d7486496e0779b
Vũ Đức Tiến <tien.vu@edn.com.vn>
2025-03-22 01:31:50 +0700

    update: clear code

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -70,3 +70,13 @@ keymap.set("n", "<leader>hS", ":Rest env set ", opts)
 
 -- Avante
 keymap.set("n", "<C-l>", ":AvanteToggle<CR>", opts) -- Toggle sidebar
+-- keymap.set("n", "<leader>ua", ":AvanteAsk<CR>", { noremap = true, silent = true }) -- Ask AI
+-- keymap.set("n", "<leader>ub", ":AvanteBuild<CR>", { noremap = true, silent = true }) -- Build dependencies
+-- keymap.set("n", "<leader>uc", ":AvanteClear<CR>", { noremap = true, silent = true }) -- Clear chat history
+-- keymap.set("n", "<C-k>", ":AvanteEdit<CR>", { noremap = true, silent = true }) -- Edit code block
+-- keymap.set("n", "<leader>uf", ":AvanteFocus<CR>", { noremap = true, silent = true }) -- Switch focus
+-- keymap.set("n", "<leader>ur", ":AvanteRefresh<CR>", { noremap = true, silent = true }) -- Refresh window
+-- keymap.set("n", "<leader>us", ":AvanteStop<CR>", { noremap = true, silent = true }) -- Stop AI request
+-- keymap.set("n", "<leader>up", ":AvanteSwitchProvider<CR>", { noremap = true, silent = true }) -- Switch AI provider
+-- keymap.set("n", "<leader>um", ":AvanteShowRepoMap<CR>", { noremap = true, silent = true }) -- Show repo map
+-- keymap.set("n", "<leader>ul", ":AvanteModels<CR>", { noremap = true, silent = true }) -- Show list of models
