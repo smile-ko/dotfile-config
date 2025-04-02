7ce092b44b92ca10b9a224554622fcaac8b71605
Vũ Đức Tiến <tien.vu@edn.com.vn>
2025-04-03 01:23:05 +0700

    update: add go vim

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -318,4 +318,19 @@ return {
       vim.treesitter.language.register("markdown", "mdx")
     end,
   },
+
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
 }
