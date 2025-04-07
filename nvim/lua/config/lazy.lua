1f2a0c1192a9ba8692b9d6c24cb7ed9d114ad5e1
Vũ Đức Tiến <tien.vu@edn.com.vn>
2025-04-07 19:33:05 +0700

    update: file

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -32,6 +32,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     -- { import = "lazyvim.plugins.extras.langtailwind" },
     { import = "lazyvim.plugins.extras.ai.copilot" },
     -- { import = "lazyvim.plugins.extras.dap.core" },
