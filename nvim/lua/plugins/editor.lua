df0bd5b31a898a3253119de4b346c9114a0d0a9c
Vũ Đức Tiến <tien.vu@edn.com.vn>
2025-05-22 22:29:51 +0700

    update: which key

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -246,4 +246,39 @@ return {
       },
     },
   },
+
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+
+    opts = {
+      icons = {
+        breadcrumb = "»",
+        separator = "󰁔", -- icon between groups
+      },
+
+      delay = function(ctx)
+        return ctx.plugin and 0 or 400
+      end,
+
+      spec = {
+        {
+          {
+            "<leader>a",
+            group = "AI Tools",
+          },
+        },
+      },
+    },
+  },
 }
