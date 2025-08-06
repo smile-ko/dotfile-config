078341f524db8794a424672a7c857c6ba009ef4e
Vũ Đức Tiến <tien.vu@edn.com.vn>
2025-08-06 21:28:17 +0700

    enable avante, copilot

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -326,7 +326,7 @@ return {
         },
         -- Displays a preview of the selected item on the current line
         ghost_text = {
-          enabled = false,
+          -- enabled = false,
         },
       },
       signature = {
