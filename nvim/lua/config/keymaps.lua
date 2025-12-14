b50df7f12f37f398dc2658481f0179011d704520
Vũ Đức Tiến <tien.vu@edn.com.vn>
2025-12-14 13:21:13 +0700

    chore: remove unused keymaps
    Drop custom mappings for increment/decrement and select-all to avoid
    conflicts with default behavior.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -19,15 +19,8 @@ keymap.set("n", "<Leader>D", '"_D')
 keymap.set("v", "<Leader>d", '"_d')
 keymap.set("v", "<Leader>D", '"_D')
 
--- Increment/decrement
-keymap.set("n", "+", "<C-a>")
-keymap.set("n", "-", "<C-x>")
-
 -- Delete a word backwards
 keymap.set("n", "dw", 'vb"_d')
-
--- Select all
-keymap.set("n", "<C-a>", "gg<S-v>G")
 
 -- Save with root permission (not working for now)
 vim.api.nvim_create_user_command("W", "w !sudo tee > /dev/null %", {})
